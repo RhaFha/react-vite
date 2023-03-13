16543e79d05c80788ce4a15db26c768a7cae6e41
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-03-13 12:15:07 +0700

    avance 13-03-2023

--- React Todo/src/App.jsx
+++ React Todo/src/App.jsx
@@ -1,32 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import CrossIcon from "./componentes/icoms/CrossIcon"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen" >
+      <header className="container mx-auto px-4 pt-8">
+        <div className="flex justify-between">
+          <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">TODO</h1>
+          <button>luna</button>
+        </div>
+        <form className="flex gap-4 items-center bg-white rounded-md overflow-hidden py-4 px-4 mt-8">
+          <spam className="rounded-full border-2 w-5 h-5 inline-block"></spam>
+          <input type='text' placeholder="Create a new todo..." className="w-full text-gray-400 outline-none"/>
+        </form>
+      </header>
+      <main className="container mx-auto mt-8 px-4 ">
+        <div className="bg-white rounded-md">
+
+          <article className="flex gap-4 py-4 items-center border-b border-b-gray-400 px-4">
+            <button className="flex-none rounded-full border-2 w-5 h-5 inline-block"></button>
+            <p className="grow text-gray-600">Curso completado</p>
+            <button className="flex-none">
+              <CrossIcon />
+            </button>
+          </article>
+          
+          <article className="flex gap-4 py-4 items-center border-b border-b-gray-400 px-4">
+            <button className="flex-none rounded-full border-2 w-5 h-5 inline-block"></button>
+            <p className="grow text-gray-600">Curso completado</p>
+            <button className="flex-none">
+              <CrossIcon />
+            </button>
+          </article>
+          
+          <article className="flex gap-4 py-4 items-center border-b border-b-gray-400 px-4">
+            <button className="flex-none rounded-full border-2 w-5 h-5 inline-block"></button>
+            <p className="grow text-gray-600">Curso completado</p>
+            <button className="flex-none">
+              <CrossIcon />
+            </button>
+          </article>
+          
+        </div>
+
+        <section>
+          <span>5 item left</span>
+          <button>Clear complete</button>
+        </section>
+      </main>
+
+      <section className="container mx-auto px-4">
+        <button>All</button>
+        <button>Activate</button>
+        <button>Complete</button>
+      </section>
+
+      <section>Drag and Drog react order</section>
     </div>
   )
 }
