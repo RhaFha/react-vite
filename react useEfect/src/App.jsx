6adcababba8702944ec0e42fae6071047b04e848
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-03-15 10:37:19 +0700

    react UseEffect terminado

--- react useEfect/src/App.jsx
+++ react useEfect/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useFetch } from './useFetch';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+  const [count, setCount] = useState(0);
+
+  const {data, loading, error} = useFetch('https://jsonplaceholder.typicode.com/users');
+
+  if(loading) return <h1>Cargando...</h1>
+  if(error) return <h1>{error}</h1>
+
+  return(
+
+    <>
+    <h1>Use effect</h1>
+    <button onClick={() => setCount(count + 1)} > Counter: {count }</button>
+    <ul>
+      { 
+        data.map( (user, index) => (
+          <li key={user.id}> {user.name}</li>
+        ))
+      }
+    </ul>
+    </>
+
   )
 }
 
-export default App
+export default App;
---
+++ react useEfect/src/useFetch.js
@@ -0,0 +1,38 @@
+import { useState, useEffect, useCallback } from "react";
+
+export const useFetch = (url) => {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
+    console.log('useFetch');
+
+    const fetchData = useCallback(async() => {
+        setLoading(true);
+
+        try{
+            const res = await fetch(url);
+
+            if(!res.ok) throw Error('Error al consumir la API')
+
+            const data = await res.json();
+            setData(data);
+        }catch (error){
+            console.log(error.message);
+            setError(error.message);
+            setData([])
+        }finally{
+            setLoading(false);
+        }
+        
+    }, [])
+
+    useEffect(() => {
+        fetchData();
+      },[])
+    
+
+    return {data, loading ,error}
+
+}
